63ba1d120d67a852f4bb7e45a7c0ea0d08c670ab
youva-cn <diyouva@gmail.com>
2024-06-28 10:03:42 +0700

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 
 def main ():
-  st.write('Minimal Example')
   st.header('This is Header')
+  st.subheader('This is SubHeader')
+  st.markdown('# Rendering Markdown ')
+  st.write('Some Phytagorean Equation : ')
+  st.lates('c^2 = a^2+b^2')
 
 if __name__ == '__main__' :
   main()
